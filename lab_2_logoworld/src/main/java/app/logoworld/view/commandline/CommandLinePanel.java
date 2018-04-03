a0ac529966ce66b42b85e8805c93e0e982521109
Anastasiya Shadrina <a.shadrina5@mail.ru>
2018-04-03 18:43:48 +0700

    Add KeyListener

--- lab_2_logoworld/src/main/java/app/logoworld/view/commandline/CommandLinePanel.java
+++ lab_2_logoworld/src/main/java/app/logoworld/view/commandline/CommandLinePanel.java
@@ -5,6 +5,8 @@ import javax.swing.event.EventListenerList;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class CommandLinePanel extends JPanel implements CommandLineCommons {
 
@@ -29,6 +31,18 @@ public class CommandLinePanel extends JPanel implements CommandLineCommons {
             public void actionPerformed(ActionEvent e) {
                 fireCommandLineEvent(new CommandLineEvent(this, commandField.getText().toUpperCase()));
             }
+        });
+        commandField.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {}
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    fireCommandLineEvent(new CommandLineEvent(this, commandField.getText().toUpperCase()));
+                }
+            }
+            @Override
+            public void keyReleased(KeyEvent e) {}
         });
 
         setLayout(new GridBagLayout());
